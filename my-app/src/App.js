efc956ae802f41b558622cf0b1f1053c24556ecc
Ivan Ushachov <90464856+VanyaSO@users.noreply.github.com>
2022-09-07 17:32:25 +0300

    Card

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,14 @@
-import './index.css';
+import Card from "./component/Card"
 
 function App() {
   return (
     <div className="App">
-
+      <Card>
+          <Card.Body>
+              <Card.Title>Title</Card.Title>
+              <Card.Text>Text</Card.Text>
+          </Card.Body>
+      </Card>
     </div>
   );
 }
---
+++ my-app/src/component/Body.jsx
@@ -0,0 +1,4 @@
+const Body = (props) => <div className="card-body">{props.children}</div>
+
+export default Body;
+
